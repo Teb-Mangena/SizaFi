ee5aa86cd8e500d38c697de1941883bd92e6ac4b
Tebatso <tebatsomangena8@gmail.com>
2025-09-21 09:17:31 +0200

    axios file fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL
+// const apiUrl = import.meta.env.VITE_API_URL
 
-console.log({apiUrl})
+// console.log({apiUrl})
 export const axiosInstance = axios.create({
-  baseURL: apiUrl,
+  baseURL: import.meta.env.VITE_API_URL || 'https://sizafi-backend.onrender.com/api',
   withCredentials: true
 });
